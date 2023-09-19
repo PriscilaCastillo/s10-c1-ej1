7d796606ecfa03b3323b970c996e63361c578f0e
Priscila Castillo <pcastillof97@gmail.com>
2023-09-18 21:08:43 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// function par_impar(number) {
+//     if (number % 2 === 0) {
+//         console.log('El numero es par');
+//     } else {
+//         console.log('El numero es impar');
+//     }
+// }
+
+// par_impar(5);
+
+let producto;
+
+const multiplicar = (number1, number2) => {
+    return number1 * number2;
+    
+}
+
+
+console.log(multiplicar(4,5));
